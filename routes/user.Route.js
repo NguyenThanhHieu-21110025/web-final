e632a2cc8e4f50caeb1b0944020918a6dd4a21ae
Nguyen Thanh Hieu <thanhhieu@192.168.1.7.non-exists.ptr.local>
2024-12-25 03:00:38 +0700

    xoa phan quyen de test

--- routes/user.Route.js
+++ routes/user.Route.js
@@ -1,14 +1,13 @@
 const userController = require("../controllers/user.Controller");
-const middlewareController = require("../middleware/auth.Middleware");
 
 const router = require("express").Router();
 
-router.get("/", middlewareController.verifyToken , userController.getAllUsers );
-router.get("/:id",middlewareController.verifyToken , userController.getUserById);
+router.get("/",  userController.getAllUsers );
+router.get("/:id", userController.getUserById);
 // router.put("/active/:id", middlewareController.verifyTokenAndAdminAuth, userController.isActive);
-router.put("/:id", middlewareController.verifyToken, userController.updateUser);  
-router.delete("/:id", middlewareController.verifyTokenAndAdminAuth, userController.deleteUser);
-router.post("/reset-password/:id", middlewareController.verifyToken, userController.resetPassword);
+router.put("/:id",  userController.updateUser);  
+router.delete("/:id",  userController.deleteUser);
+router.post("/reset-password/:id",  userController.resetPassword);
 
 
 module.exports = router;
