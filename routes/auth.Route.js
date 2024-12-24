ba5712d34aadbd19c5d6cc37937e5998ae5980ac
Nguyen Thanh Hieu <thanhhieu@192.168.1.7.non-exists.ptr.local>
2024-12-25 01:32:47 +0700

    updated router

--- routes/auth.Route.js
+++ routes/auth.Route.js
@@ -1,38 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.Controller");
-const {body, validationResult} = require("express-validator"); // Ensure 'express-validator' is installed
+const { body, validationResult } = require("express-validator"); // Ensure 'express-validator' is installed
+const passport = require("../controllers/passport");
 
-// Đăng ký người dùng
-router.post(
-    "/register",
-    [
-        body("email").isEmail().withMessage("Please enter a valid email"),
-        body("password").isLength({min: 6}).withMessage("Password must be at least 6 characters")
-    ],
-    async (req, res) => {
-        // Validate request body
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({errors: errors.array()});
-        }
-
-        await authController.registerUser(req, res);
-    }
-);
-
-// Đăng nhập người dùng
-router.post("/login", async (req, res) => {
-    await authController.loginUser(req, res);
+// Route đăng ký người dùng
+router.get('/register', (req, res) => {
+    res.render('guest/register'); // Hiển thị form đăng ký
 });
-router.get("/login", async (req, res) => {
-    return res.render('guest/login');
-})
 
-// Yêu cầu Refresh Token
-router.post("/refresh", async (req, res) => {
-    await authController.requestRefreshToken(req, res);
+router.post('/register', authController.registerUser); // Xử lý đăng ký
+
+
+router.get('/login', (req, res) => {
+    res.render('guest/login');
 });
+
+router.post('/login', authController.loginUser);
+
+
+
 
 // Đăng xuất người dùng
 router.post("/logout", async (req, res) => {
@@ -43,5 +30,23 @@ router.post("/logout", async (req, res) => {
 router.delete("/delete/:id", async (req, res) => {
     await authController.deleteUser(req, res);
 });
+
+// Đăng nhập qua Facebook
+router.get('/facebook', passport.authenticate('facebook'));
+
+// Callback sau khi đăng nhập qua Facebook
+router.get('/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/',
+    successRedirect: '/dashboard'
+}));
+
+// Đăng nhập qua Google
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Callback sau khi đăng nhập qua Google
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: '/',
+    successRedirect: '/dashboard'
+}));
 
 module.exports = router;
